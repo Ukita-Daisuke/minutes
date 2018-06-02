c08c60046675d7836199c6608d855a4d834fc375
Daisuke Ukita <black0725dog@yahoo.co.jp>
2018-06-02 18:57:03 +0900

    途中まで完成

--- js/minutes.js
+++ js/minutes.js
@@ -3,4 +3,7 @@ $("#category_id").change(function(){
   $("#hidearea").show();
 });
 
-$("")
+$("#plus").click(function(){
+  $("#player_name").clone.appendTo("#player_name");
+  $("#player_role").clone.appendTo("#player_role");
+});
